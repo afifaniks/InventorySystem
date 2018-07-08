a509e4bbf4bca9179e1b17c874f7fcd3f3d36c15
Afif Al Mamun <afifanik@gmail.com>
2018-07-09 03:11:59 +0600

    Initiating Tesla Rental Inventory RDBMS Project.

---
+++ src/controller/Base.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Author: Afif Al Mamun
+ * Written on: 08-Jul-18
+ * Project: TeslaRentalInventory
+ **/
+public class Base {
+
+
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+/**
+ * Author: Afif Al Mamun
+ * Written on: 08-Jul-18
+ * Project: TeslaRentalInventory
+ **/
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/base.fxml"));
+        Scene scene = new Scene(root);
+
+        String css = this.getClass().getResource("/css/base.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        primaryStage.setTitle("Tesla Rental Inventory");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
