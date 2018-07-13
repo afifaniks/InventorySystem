b4645445da1eda1d00dfe690c8c68519d051296b
Afif Al Mamun <afifanik@gmail.com>
2018-07-13 15:03:39 +0600

    Customer layout changed, different fields added

--- src/controller/Customer.java
+++ src/controller/Customer.java
@@ -1,5 +1,8 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.effect.DropShadow;
@@ -18,12 +21,74 @@ import java.util.ResourceBundle;
  **/
 public class Customer implements Initializable {
 
+    private static boolean editToggle = false;
+
     @FXML
     private Circle imgCustomerPhoto;
+
+    @FXML
+    private JFXButton btnEditMode;
+
+    @FXML
+    private JFXTextField customerID;
+
+    @FXML
+    private JFXTextField phone;
+
+    @FXML
+    private JFXTextField firstName;
+
+    @FXML
+    private JFXTextField lastName;
+
+    @FXML
+    private JFXTextField address;
+
+    @FXML
+    private JFXTextField email;
+
+    @FXML
+    private JFXTextField gender;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ImagePattern img = new ImagePattern(new Image("/resource/icons/10407479_1396350623998299_689954862227931112_n.jpg"));
         imgCustomerPhoto.setFill(img);
+
+        imgCustomerPhoto.setOnMouseClicked(event -> {
+            if(editToggle) {
+                System.out.println("Yes");
+            }
+        });
+
+    }
+
+    @FXML
+    void btnEditModeToggle(ActionEvent event) {
+        if(editToggle) {
+            editToggle = false;
+            btnEditMode.setStyle("");
+            customerID.setEditable(false);
+            phone.setEditable(false);
+            firstName.setEditable(false);
+            lastName.setEditable(false);
+            address.setEditable(false);
+            email.setEditable(false);
+            gender.setEditable(false);
+
+
+        } else {
+            editToggle = true;
+            btnEditMode.setStyle("-fx-background-color: green");
+
+            customerID.setEditable(true);
+            phone.setEditable(true);
+            firstName.setEditable(true);
+            lastName.setEditable(true);
+            address.setEditable(true);
+            email.setEditable(true);
+            gender.setEditable(true);
+        }
     }
 }
